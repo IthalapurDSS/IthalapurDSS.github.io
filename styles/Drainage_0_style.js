c3fc92bd0837154a66eba127382ab7781672b3e5
IthalapurDSS <varmasenthil495@gmail.com>
2023-10-09 15:34:43 +0530

    added new files

---
+++ styles/Drainage_0_style.js
@@ -0,0 +1,88 @@
+var size = 0;
+var placement = 'point';
+function categories_Drainage_0(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                switch(value.toString()) {default:
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(115,179,216,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 2}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '6':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(115,179,216,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 2}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '5':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(115,179,216,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 2}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '4':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(115,179,216,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 2}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '3':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(8,48,107,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 2}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '2':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(21,99,170,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 2}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '1':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(21,99,170,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 2}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_Drainage_0 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("id");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'line';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+var style = categories_Drainage_0(feature, value, size, resolution, labelText,
+                          labelFont, labelFill, bufferColor,
+                          bufferWidth, placement);
+
+    return style;
+};
