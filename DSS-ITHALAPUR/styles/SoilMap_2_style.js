0581bcabd2b610f214c6a9c0ad06d8c13320a6b2
IthalapurDSS <varmasenthil495@gmail.com>
2023-12-25 20:50:26 +0530

    new Dss

--- DSS-ITHALAPUR/styles/SoilMap_2_style.js
+++ DSS-ITHALAPUR/styles/SoilMap_2_style.js
@@ -1,9 +1,17 @@
 var size = 0;
 var placement = 'point';
-function categories_SoilMap_2(feature, value, size, resolution, labelText,
+function categories_SOILMAP_2(feature, value, size, resolution, labelText,
                        labelFont, labelFill, bufferColor, bufferWidth,
                        placement) {
-                switch(value.toString()) {case 'Calcic Haplustepts':
+                switch(value.toString()) {case '410':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(16,92,11,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '415':
                     return [ new ol.style.Style({
         stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(31,31,112,1.0)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
@@ -11,29 +19,21 @@ function categories_SoilMap_2(feature, value, size, resolution, labelText,
                               bufferWidth)
     })];
                     break;
-case 'Typic Haplusterts':
+case '420':
                     return [ new ol.style.Style({
         stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(93,31,32,1.0)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })];
-                    break;
-case 'Typic Ustorthents':
-                    return [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(16,92,11,1.0)'}),
-        text: createTextStyle(feature, resolution, labelText, labelFont,
-                              labelFill, placement, bufferColor,
-                              bufferWidth)
-    })];
                     break;}};
 
-var style_SoilMap_2 = function(feature, resolution){
+var style_SOILMAP_2 = function(feature, resolution){
     var context = {
         feature: feature,
         variables: {}
     };
-    var value = feature.get("Soil Type");
+    var value = feature.get("Elev");
     var labelText = "";
     size = 0;
     var labelFont = "10px, sans-serif";
@@ -48,7 +48,7 @@ var style_SoilMap_2 = function(feature, resolution){
         labelText = String("");
     }
     
-var style = categories_SoilMap_2(feature, value, size, resolution, labelText,
+var style = categories_SOILMAP_2(feature, value, size, resolution, labelText,
                           labelFont, labelFill, bufferColor,
                           bufferWidth, placement);
 
